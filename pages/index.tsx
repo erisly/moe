806b1906edf7e2f53d5a5352f552d34033c49782
ibx34 <guamjust@gmail.com>
2021-12-23 21:52:40 -0500

    chore: lint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import githubIcon from '@iconify/icons-simple-icons/github';
 import type { NextPage } from 'next';
-import erisly from "../public/erisly.png";
 import Image from 'next/image';
 
 import { Button, Constants, Head } from '../components';
+import erisly from '../public/erisly.png';
 
 const Page: NextPage = () => {
     return (
@@ -12,7 +12,7 @@ const Page: NextPage = () => {
 
             <main className="flex flex-col items-center justify-center flex-1 min-h-screen px-8 text-center">
                 <div className="sm:mx-24 leading-0 object-fit relative h-72 w-72 sm:h-96 sm:w-96 md:h-[530px] md:w-[500px]">
-                    <Image alt="Erisly" className="rounded-md" quality="100" src={erisly} layout="fill" />
+                    <Image alt="Erisly" className="rounded-md" layout="fill" quality="100" src={erisly} />
                 </div>
                 <h1 className="pt-4 font-bold text-3xl sm:text-5xl md:text-8xl">Erisly.moe</h1>
                 <p className="pt-8">{Constants.DESCRIPTION}</p>
