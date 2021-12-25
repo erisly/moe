367e949764e0bc4b4b2bb21d1275d4ef012ac272
PikaDude <mail@pikadude.me>
2021-12-25 23:07:13 +1100

    clean button code a bit
    not sure how im supposed to actually make this look nice
    tailwind moment

--- components/Button.tsx
+++ components/Button.tsx
@@ -17,22 +17,19 @@ export default function Button(props: {
     noExternal?: boolean;
     subClassName?: string;
 }): ReactElement {
+    const linkClassName = `group ${props.className}`;
+    const containerClassName = `flex items-center p-3 ${props.hoverToShowContent ? '' : 'space-x-3'} no-underline hover:button button rounded-3xl ${
+        props.subClassName
+    }`;
+    const containerStyle = { backgroundColor: props.colour?.bg, color: props.colour?.fg };
+    const contentClassName = props.hoverToShowContent ? 'max-w-0 transition-slide group-hover:max-w-sm group-hover:ml-3' : undefined;
+    const contentStyle = props.hoverToShowContent ? { clipPath: 'polygon(0% 0, 100% 0, 100% 100%, 0 100%)', color: 'black' } : undefined;
+
     return (
-        <Link button className={`group ${props.className}`} href={props.href} noExternal={props.noExternal}>
-            <div
-                className={`flex items-center p-3 ${props.hoverToShowContent ? '' : 'space-x-3'} no-underline hover:button button rounded-3xl ${
-                    props.subClassName
-                }`}
-                style={{
-                    backgroundColor: props.colour?.bg,
-                    color: props.colour?.fg,
-                }}
-            >
+        <Link button className={linkClassName} href={props.href} noExternal={props.noExternal}>
+            <div className={containerClassName} style={containerStyle}>
                 {props.icon ? <InlineIcon height={props.iconSize || 16} icon={props.icon} width={props.iconSize || 16} /> : null}
-                <span
-                    className={props.hoverToShowContent ? 'max-w-0 transition-slide group-hover:max-w-sm group-hover:ml-3' : undefined}
-                    style={props.hoverToShowContent ? { clipPath: 'polygon(0% 0, 100% 0, 100% 100%, 0 100%)', color: 'black' } : undefined}
-                >
+                <span className={contentClassName} style={contentStyle}>
                     {props.content}
                 </span>
             </div>
