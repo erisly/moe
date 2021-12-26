7f2f2ce776900c4686505b08797e4f430052ea64
PikaDude <mail@pikadude.me>
2021-12-26 23:13:50 +1100

    remove useless class

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ const Page: NextPage = () => {
             <Head />
 
             <main className="flex flex-col items-center justify-center flex-1 min-h-screen px-8 text-center">
-                <div className="sm:mx-24 leading-0 object-fit relative h-72 w-72 sm:h-96 sm:w-96 md:h-[530px] md:w-[500px]">
+                <div className="sm:mx-24 object-fit relative h-72 w-72 sm:h-96 sm:w-96 md:h-[530px] md:w-[500px]">
                     <Image alt="Erisly" className="rounded-md" layout="fill" quality="100" src={erisly} />
                 </div>
                 <h1 className="pt-4 text-3xl font-bold sm:text-5xl md:text-8xl">Erisly.moe</h1>
