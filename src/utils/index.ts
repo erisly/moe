7e26001284184b48e633cefbe6fa5dbbf47420a4
PikaDude <mail@pikadude.me>
2023-03-25 22:00:52 +1100

    add meta

---
+++ src/utils/index.ts
@@ -0,0 +1,2 @@
+export const Description =
+    'The ultimate shrine to the most enchanting and cutest Goddess, Erisly, where you can bask in her glory and join a community of worshippers who know that Erisly is the one true waifu.';
